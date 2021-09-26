053920f2d53994cccdfd9acf96f66be287684c19
sergio1999ram <sramirezpch@gmail.com>
2021-09-26 03:24:26 -0500

    header navbar y main content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MainContentComponent } from './home/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
+    InicioComponent,
+    MainContentComponent,
   ],
   imports: [
     BrowserModule
