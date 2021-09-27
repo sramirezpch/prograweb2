4a97868fed706395171807973a23de717f576e27
sergio1999ram <sramirezpch@gmail.com>
2021-09-26 21:05:31 -0500

    banner changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from './common/navbar/navbar.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { MainContentComponent } from './home/main-content/main-content.component';
+import { BannerComponent } from './home/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MainContentComponent } from './home/main-content/main-content.component
     FooterComponent,
     InicioComponent,
     MainContentComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule
