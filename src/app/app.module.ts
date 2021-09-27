3afd95e30cef06ba6ae75bf966ec9ec3dbf40027
eduardo <eduardo.salinas3498@gmail.com>
2021-09-27 13:33:51 -0500

    Avance cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CardComponent } from './entities/card/card.component';
+import { EntitiesComponent } from './entities/entities.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './commom/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    CardComponent,
+    EntitiesComponent,
+    ServicesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/entities/cards.ts
@@ -0,0 +1,14 @@
+import { Card } from "./card";
+
+export const CARDS: Card[] = [
+
+  {id_estadio: 1,nombre: 'Estadio Nacional del Perú',ciudad: 'Lima',capacidad: '50000',imagen: 'assets/nacional.jpg'},
+  {id_estadio: 2,nombre: 'Estadio Monumental de River',ciudad: 'Buenos Aires',capacidad: '72054',imagen: 'assets/river.jpg'},
+  {id_estadio: 3,nombre: 'Estadio Mané Garrincha',ciudad: 'Brasilia',capacidad: '72800',imagen: 'assets/mane.jpg'},
+  {id_estadio: 4,nombre: 'Estadio Nacional Julio Martínez Prádanos',ciudad: 'Santiago de Chile',capacidad: '48665',imagen: 'assets/pradanos.jpg'},
+  {id_estadio: 5,nombre: 'Estadio Olímpico Atahualpa',ciudad: 'Quito',capacidad: '35258',imagen: 'assets/atahualpa.jpg'},
+  {id_estadio: 6,nombre: 'Estadio Metropolitano Roberto Meléndez',ciudad: 'Barranquilla',capacidad: '46692',imagen: 'assets/metropolitano.jpg'},
+  {id_estadio: 7,nombre: 'Estadio Centenario',ciudad: 'Montevideo',capacidad: '60235',imagen: 'assets/centenario.jpg'},
+  {id_estadio: 8,nombre: 'Estadio Defensores del Chaco',ciudad: 'Asunción',capacidad: '42000',imagen: 'assets/chaco.jpg'},
+  {id_estadio: 9,nombre: 'Estadio Hernando Siles',ciudad: 'La Paz',capacidad: '41143',imagen: 'assets/lapaz.jpg'}
+]
