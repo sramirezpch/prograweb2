a776e5cc273528e7f64202c3ba73dda06de2110e
eduardo <eduardo.salinas3498@gmail.com>
2021-09-27 13:59:39 -0500

    Routing agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardsComponent } from './components/cards/cards.component';
-import { CardComponent } from './entities/card/card.component';
-import { EntitiesComponent } from './entities/entities.component';
-import { ServicesComponent } from './services/services.component';
+import { CardsComponent } from './home/cards/cards.component';
 import { FooterComponent } from './commom/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent,
-    CardComponent,
-    EntitiesComponent,
-    ServicesComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
