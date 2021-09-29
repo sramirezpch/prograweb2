931b8e6c76051de67cd7c9e3e7620ca22eaa6058
rodoleon51 <rodoleond@icloud.com>
2021-09-29 14:05:29 -0500

    Avance 2: Campeones añadidos

--- src/app/entities/campeones.ts
+++ src/app/entities/campeones.ts
@@ -1,16 +1,17 @@
 import { Campeon } from "./campeon";
 
 export const CAMPEONES: Campeon[]=[
-    {id: 1,deporte: "Gerente", nombre: "Peter Parker", edad: 19, 	pais: " ",  foto:"assets/campeones/peterparker.jpg" },
-    {id: 2,deporte: "producción", nombre: "Tony Stark", 	edad: 39, pais: " ",  foto:"assets/campeones/tonystark.jpg" },
-    {id: 3,deporte: " Seguridad", nombre: "Bruce Banner", edad: 32, pais: " ",  foto:"assets/campeones/brucebanner.jpg" },
-    {id: 4,deporte: "Investig", nombre: "Susan Storm", edad: 28, pais: " ",  foto:"assets/campeones/susanstorm.jpg" },
-    {id: 5,deporte: "sistemas", nombre: "Natasha Romanoff", edad:  24, pais: " ",  foto:"assets/campeones/natasha.jpg" },
-    {id: 6,deporte: "Asistente", nombre: "Pepper Potts", edad:  31, pais: " ",  foto:"assets/campeones/pepperpotts.jpg" },
-    {id: 7,deporte: "Vendedor", nombre: "Clark Kent", edad: 36, pais: " ",  foto:"assets/campeones/clarkkent.jpg" },
-    {id: 8,deporte: "Contador", nombre: "Bruce Wayne", 	edad: 27, pais: " ",  foto:"assets/campeones/brucewayne.jpg" },
-    {id: 9,deporte: "Almacén", nombre: "Donald Blake", edad: 28, pais: " ",  foto:"assets/campeones/donaldblake.jpg" },
-    {id: 10,deporte: "General", nombre: "Diana Prince", edad: 26, pais: " ",  foto:"assets/campeones/dianaprince.jpg" },
-    {id: 11,deporte: "Vendedor",	nombre: "Steve Rogers", edad: 27, pais: " ",  foto:"assets/campeones/steve-rogers.jpg" },
-    {id: 12,deporte: " Planta", nombre: "Wanda Maximoff", edad: 33, pais: " ",  foto:"assets/campeones/wandamaximoff.jpg" }    
+    {id: 1,deporte: "Tiro al Arco", apellido: "GAZOZ", nombre: "Mete", sexo:"M", edad: 22, 	evento: "Individual", pais: "Turquía",  foto:"assets/campeones/mete_gazoz.jpg" },
+    {id: 2,deporte: "Tiro al Arco", apellido: "AN", nombre: "San", sexo:"F", edad: 20, 	evento: "Individual", pais: "República de Corea ",  foto:"assets/campeones/san_an.jpg" },
+    {id: 3,deporte: "Atletismo", apellido: "BAREGA", nombre: "Selemon", sexo:"M", edad: 21, evento: "10,000m", pais: "Etiopía",  foto:"assets/campeones/selemon_barega.jpg" },
+    {id: 4,deporte: "Atletismo", apellido: "HASSAN", nombre: "Sifan", sexo:"F", edad:28,evento: "10,000m", pais: "Paises Bajos",  foto:"assets/campeones/sifan_hassan.jpg" },    
+    {id: 5,deporte: "Atletismo", apellido: "JACOBS", nombre: "Lamont", sexo:"M", edad: 26, evento: "100m", pais: "Italia",  foto:"assets/campeones/lamont_jacobs.jpg" },
+    {id: 6,deporte: "Atletismo", apellido: "THOMPSON", nombre: "Elaine", sexo:"F", edad: 29,evento: "100m", pais: "Jamiaca",  foto:"assets/campeones/elaine_thompson.jpg" },
+    {id: 7,deporte: "Atletismo", apellido: "PARCHMENT", nombre: "Hansle", sexo:"M", edad: 24, evento: "110m con vallas", pais: "Jamaica", foto:"assets/campeones/hansle_parchment.jpg" },
+    {id: 8,deporte: "Atletismo", apellido: "Camacho", nombre: "Jasmine", sexo:"F", edad: 24, evento: "100m con vallas", pais: "Puerto Rico",  foto:"assets/campeones/elaine_camacho.jpg" },
+    {id: 9,deporte: "Atletismo", apellido: "INGEBRIGTSEN", nombre: "Jakob", sexo:"M", edad: 20, evento: "1500m", pais: "Noruega",  foto:"assets/campeones/jakob_ingebrigtsen.jpg" },
+    {id: 10,deporte: "Atletismo", apellido: "KIPYEGON", nombre: "Faith", sexo:"F", edad: 27,evento: "1500m", pais: "Kenya",  foto:"assets/campeones/faith_kipyegon.jpg" },
+    {id: 11,deporte: "Ciclismo", apellido: "CARAPAZ", nombre: "Richard", sexo:"M", edad: 28, 	evento: "Pista", pais: " Ecuador",  foto:"assets/campeones/richard_carapaz.jpg" },
+    {id: 12,deporte: "Ciclismo", apellido: "KIESENHOFER", nombre: "Anna", sexo:"F", edad: 30, 	evento: "Pista", pais: "Austria",  foto:"assets/campeones/anna_kiesenhofer.jpg" },
+    
 ]
