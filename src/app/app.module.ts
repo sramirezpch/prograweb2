0291a3e517b19720fb7246874762041c9f1b468f
Sergio Ramirez <45132050+sergio1999ram@users.noreply.github.com>
2021-10-14 18:07:56 -0500

    Revert "Rodolfo"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { FooterComponent } from './common/footer/footer.component';
-import { CampeonesComponent } from './components/campeones/campeones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    CampeonesComponent,
   ],
   imports: [
     BrowserModule
